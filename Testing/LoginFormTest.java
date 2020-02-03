f9dea93d960c59f54d8c6170e0ec861efbe73d85
mohammedmiah67890@gmail.com <mohammedmiah67890@gmail.com>
2020-02-03 16:24:44 +0000

    Added Testing methods

---
+++ Testing/LoginFormTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginFormTest {
+
+    @Test
+    public void openOfficeManagerForm() {
+    }
+
+    @Test
+    public void openTicketAdvisorForm() {
+    }
+
+    @Test
+    public void openTravelAdvisorForm() {
+    }
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+}
