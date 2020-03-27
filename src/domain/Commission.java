87d835e514609e06a5bf983b9f5b42c113cc2aff
mohammedmiah67890@gmail.com <mohammedmiah67890@gmail.com>
2020-03-27 13:21:50 +0000

    imcomplete generate turnover report

--- src/domain/Commission.java
+++ src/domain/Commission.java
@@ -3,11 +3,11 @@ package domain;
 public class Commission {
     private int commission_id;
     private Float commission_rate;
-    private Boolean active;
-    public Commission(int commission_id, Float commission_rate, Boolean active) {
+    private int type;
+    public Commission(int commission_id, Float commission_rate, int type) {
         this.commission_id = commission_id;
         this.commission_rate = commission_rate;
-        this.active = active;
+        this.type = type;
     }
 
     public int getCommission_id() {
@@ -18,12 +18,12 @@ public class Commission {
         return commission_rate;
     }
 
-    public Boolean getActive() {
-        return active;
+    public int getType() {
+        return type;
     }
     public Object[] rowArray(){
         return new Object[] {getCommission_id(),
                 getCommission_rate(),
-                getActive()};
+                getType()};
     }
 }
